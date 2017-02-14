09ebaac265321b98e13be4c4bd65e332f9ef8759
Peter Mösenthin <moesenthin@sipgate.de>
2017-02-14 15:29:59 +0100

    add stories for custom start times

--- stories/timers/Stopwatch.story.js
+++ stories/timers/Stopwatch.story.js
@@ -5,4 +5,20 @@ import Stopwatch from '../../src/timers/Stopwatch';
 storiesOf('Timers', module)
 	.add('StopWatch', () => (
 		<Stopwatch />
-	));
+	))
+	.add('StopWatch with custom time at 1:40', () => {
+		const startTime = new Date();
+		const aMinuteAgo = startTime.getTime() - 100000;
+		startTime.setTime(aMinuteAgo);
+		return (
+			<Stopwatch start={startTime}/>
+		);
+	})
+	.add('StopWatch with custom time hour at 2:46:40', () => {
+		const startTime = new Date();
+		const aMinuteAgo = startTime.getTime() - 10000000;
+		startTime.setTime(aMinuteAgo);
+		return (
+			<Stopwatch start={startTime}/>
+		);
+	});
